60fce6c3446c0d64252432a187d94041a59bf496
vinicius Puente <vinicius.puente@db1.com.br>
2020-07-22 19:11:10 -0300

    add images for  each section

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,48 @@
-let liEls = document.querySelectorAll('ul li');
+/* series carousel */
+
+let liEls = document.querySelectorAll('li.carousel__list-item.carousel__list-item--series');
+let index = 0;
+window.show = function(increase) {
+  index = index + increase;
+  index = Math.min(
+    Math.max(index,0),
+    liEls.length-1
+  );
+  liEls[index].scrollIntoView({behavior: 'smooth'});
+}
+
+/* animes carousel */
+
+let liEls = document.querySelectorAll('li.carousel__list-item.carousel__list-item--animes');
+let index = 0;
+window.show = function(increase) {
+  index = index + increase;
+  index = Math.min(
+    Math.max(index,0),
+    liEls.length-1
+  );
+  liEls[index].scrollIntoView({behavior: 'smooth'});
+}
+
+
+/* mais recente carousel */
+
+let liEls = document.querySelectorAll('li.carousel__list-item.carousel__list-item--mais-recente');
+let index = 0;
+window.show = function(increase) {
+  index = index + increase;
+  index = Math.min(
+    Math.max(index,0),
+    liEls.length-1
+  );
+  liEls[index].scrollIntoView({behavior: 'smooth'});
+}
+
+
+
+/* minha-lista carousel */
+
+let liEls = document.querySelectorAll('li.carousel__list-item.carousel__list-item--minha-lista');
 let index = 0;
 window.show = function(increase) {
   index = index + increase;
