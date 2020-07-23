99a19b1b6e678924d874e4ad494dc203728c6d63
vinicius Puente <vinicius.puente@db1.com.br>
2020-07-23 09:31:11 -0300

    adiciona carousel

--- public/js/index.js
+++ public/js/index.js
@@ -1,50 +1,9 @@
 /* series carousel */
 
-let liEls = document.querySelectorAll('li.carousel__list-item.carousel__list-item--series');
 let index = 0;
-window.show = function(increase) {
-  index = index + increase;
-  index = Math.min(
-    Math.max(index,0),
-    liEls.length-1
-  );
-  liEls[index].scrollIntoView({behavior: 'smooth'});
-}
-
-/* animes carousel */
-
-let liEls = document.querySelectorAll('li.carousel__list-item.carousel__list-item--animes');
-let index = 0;
-window.show = function(increase) {
-  index = index + increase;
-  index = Math.min(
-    Math.max(index,0),
-    liEls.length-1
-  );
-  liEls[index].scrollIntoView({behavior: 'smooth'});
-}
-
-
-/* mais recente carousel */
-
-let liEls = document.querySelectorAll('li.carousel__list-item.carousel__list-item--mais-recente');
-let index = 0;
-window.show = function(increase) {
-  index = index + increase;
-  index = Math.min(
-    Math.max(index,0),
-    liEls.length-1
-  );
-  liEls[index].scrollIntoView({behavior: 'smooth'});
-}
-
-
-
-/* minha-lista carousel */
-
-let liEls = document.querySelectorAll('li.carousel__list-item.carousel__list-item--minha-lista');
-let index = 0;
-window.show = function(increase) {
+window.show = function(increase, type) {
+  const classItens = `li.carousel__list-item.carousel__list-item--${type}`
+  let liEls = document.querySelectorAll(classItens);
   index = index + increase;
   index = Math.min(
     Math.max(index,0),
