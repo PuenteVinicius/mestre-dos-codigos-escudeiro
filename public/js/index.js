bfdf7f002a6df6c1e4debb8a4a1f0725f9d8ae0d
vinicius Puente <vinicius.puente@db1.com.br>
2020-07-23 22:07:11 -0300

    add menu opacity scroll

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,9 @@
-/* series carousel */
+const userNavigateClass = 'user__navigate--show';
+const headerClass = 'header--opacity';
 
 let index = 0;
+/* series carousel */
+
 function show(increase, type) {
   const classItens = `li.carousel__list-item.carousel__list-item--${type}`
   let liEls = document.querySelectorAll(classItens);
@@ -14,7 +17,6 @@ function show(increase, type) {
 
 /* showNavegar */
 
-const userNavigateClass = 'user__navigate--show';
 
 function showNavegar() {
   const element =  document.getElementById('navigate');
@@ -24,3 +26,23 @@ function showNavegar() {
   }
   return element.classList.add(userNavigateClass);
 }
+
+/* opacity on scroll */
+
+window.onscroll = function (e) {
+  const doc = document.documentElement;
+  const top = (window.pageYOffset || doc.scrollTop)  - (doc.clientTop || 0);
+  const element = document.getElementById('header');
+
+  if(top >= 200) {
+    if(!element.classList.contains(headerClass)) {
+      return element.classList.add(headerClass);
+    }
+  } 
+  else {
+    if(element.classList.contains(headerClass)) {
+      return element.classList.remove(headerClass);
+    }
+  }
+} 
+
